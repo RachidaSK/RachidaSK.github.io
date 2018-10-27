be085b053081f2d744b1e0a33b9dc0c604c5d03c
RachidaSK <rachida.koco@gmail.com>
2018-10-26 22:00:30 -0400

    Update portfolio

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,10 @@
 
 const sendEmail = function () {
-    const form = $('#email-form');
+    const form = $('.email-form');
     form.addClass('send').attr('action', "mailto:rachida.koco@gmail.com");
     form.attr("method", "post");
     form.attr("enctype", "text/plain");
 };
 
 $('#email').on('click', sendEmail);
+$('#email-logo').on('click', sendEmail);
