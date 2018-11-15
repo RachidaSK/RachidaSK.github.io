2495079aa8d8496d478089740e3483b660823597
RachidaSK <rachida.koco@gmail.com>
2018-11-14 21:34:44 -0500

    Write function navbar background on scroll and add items to my portfolio

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,37 @@
+$(document).ready(function () {
+    // This function changes the navbar bacground when on different sections of the website
+    $('.thisScroll').scrollspy({ target: '#navbar-main' });
 
-const sendEmail = function () {
-    const form = $('.email-form');
-    form.addClass('send').attr('action', "mailto:rachida.koco@gmail.com");
-    form.attr("method", "post");
-    form.attr("enctype", "text/plain");
-};
+    const top1 = $('#no-bkg').offset().top;
+    const about = $('#about').offset().top;
+    const portfolio = $('#portfolio').offset().top;
+    const contact = $('#contact').offset().top;
 
-$('#email').on('click', sendEmail);
-$('#email-logo').on('click', sendEmail);
+    $(document).scroll(function () {
+        const scrollPos = $(document).scrollTop();
+        if (scrollPos >= top1 && scrollPos < about) {
+            $('#navbar-main').css('background', 'none');
+        } else if (scrollPos >= about && scrollPos < portfolio) {
+            $('#navbar-main').css('background-color', '#ffffff');
+        } else if (scrollPos >= portfolio) {
+            $('#navbar-main').css('background-color', '#000000');
+        }
+    });
+
+
+    // This function handles button events
+
+    const sendEmail = function () {
+        const form = $('.email-form');
+        form.addClass('send').attr('action', "mailto:rachida.koco@gmail.com");
+        form.attr("method", "post");
+        form.attr("enctype", "text/plain");
+    };
+
+    $('#email').on('click', sendEmail);
+    $('#email-logo').on('click', sendEmail);
+});
+
+
+
+
